a3b104d400529037119f4819b2a6c75ef64293e4
Rain <1915434082@qq.com>
2021-11-22 21:09:22 +0800

    更新

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author Rain
+ * @version 1.0
+ * @date 2021/11/22 21:00
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
